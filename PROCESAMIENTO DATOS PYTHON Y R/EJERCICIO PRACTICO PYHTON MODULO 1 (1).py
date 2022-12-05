36ed3f126e4413eca5bf2ddd325f5c70f8fef7ec
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2022-12-05 19:17:19 +0100

    Update EJERCICIO PRACTICO PYHTON MODULO 1 (1).py

--- PROCESAMIENTO DATOS PYTHON Y R/EJERCICIO PRACTICO PYHTON MODULO 1 (1).py
+++ PROCESAMIENTO DATOS PYTHON Y R/EJERCICIO PRACTICO PYHTON MODULO 1 (1).py
@@ -5,6 +5,29 @@
 
 
 # PRUEBA FINAL HERRAMIENTAS DEL CIENTIFICO DE DATOS
+"""
+
+El Informe Mundial de la Felicidad es una encuesta histórica sobre el estado de la felicidad global. El primer informe se publicó en 2012, el segundo en 2013,
+el tercero en 2015 y el cuarto en la actualización de 2016. El World Happiness 2017, que clasifica a 155 países según sus niveles de felicidad, se lanzó en las Naciones Unidas en un evento que celebra el día internacional de la felicidad el 20 de marzo. El informe continúa ganando reconocimiento mundial a medida que los gobiernos, las organizaciones y la sociedad civil utilizan cada vez más los indicadores de felicidad para informar de sus decisiones de formulación de políticas. Los principales expertos en todos los campos (economía, psicología, análisis de encuestas, estadísticas nacionales, salud, políticas públicas y más)
+describen cómo las mediciones de bienestar se pueden usar de manera efectiva para evaluar el progreso de las naciones.
+Sobre los informes de felicidad de 2015 y 2016, realizar las siguientes exploraciones:
+
+Cargar los dos CSV como datasets.
+Identificra las columnas de ambos datasets: ¿hay diferencias entre ambos?
+Une ambos dataframes, sin importar que los dos compartan las mismas diferencias.
+Revisa el número de nulos que hay por cada columna, así como su porcentaje.
+Cambia los valores nulos de las columnas "Lower Confidence Interval" y "Upper Confidence Interval" por un número aleatorio entre el valor mínimo y máximo de la misma columna (un único número, no es necesario uno diferente para cada fila con valor nulo).
+Cambia los valores nulos de la columna "Standard Error" por su media al cuadrado.
+Obtén un resumen estadístico del dataframe sin valores nulos.
+Muestra de forma gráfica la relación entre la familia y la salud.
+Muestra de forma gráfica la relación entre la puntuación de felicidad y la confianza (corrupción del gobierno).
+Muestra la matriz de correlación del daframe.
+Tras unir los dataframes, los países aparecerán más de una vez. Muestra agrupado el dataframe por país con el valor máximo de felicidad, sin importar el año.
+¿Tiene relación la felicidad con la generosidad? Muéstralo gráficamente a través de la puntuación de libertad.
+Muestra la distribución del grado de distopía en función de la región.
+
+"""
+
 ## Ejercicio 1 - Python
 ### Se pide: 
 
